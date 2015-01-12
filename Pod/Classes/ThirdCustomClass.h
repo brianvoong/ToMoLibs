da03da0d574992cbf4a5be899977d2f1df6c9a17
Brian Voong <brian@touchofmodern.com>
2015-01-12 14:29:00 -0800

    ThirdCustomClass

---
+++ Pod/Classes/ThirdCustomClass.h
@@ -0,0 +1,13 @@
+//
+//  ThirdCustomClass.h
+//  Pods
+//
+//  Created by Brian Voong on 1/12/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThirdCustomClass : NSObject
+
+@end
---
+++ Pod/Classes/ThirdCustomClass.m
@@ -0,0 +1,13 @@
+//
+//  ThirdCustomClass.m
+//  Pods
+//
+//  Created by Brian Voong on 1/12/15.
+//
+//
+
+#import "ThirdCustomClass.h"
+
+@implementation ThirdCustomClass
+
+@end
