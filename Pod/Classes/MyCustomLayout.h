8407af7b174da32964616749d0e26d9f70cd4d23
Brian Voong <brian@touchofmodern.com>
2015-01-12 14:23:15 -0800

    MyCustomLayout in pods dir

---
+++ Pod/Classes/MyCustomLayout.h
@@ -0,0 +1,15 @@
+//
+//  MyCustomLayout.h
+//  Pods
+//
+//  Created by Brian Voong on 1/12/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyCustomLayout : NSObject
+
+- (void)doWork;
+
+@end
---
+++ Pod/Classes/MyCustomLayout.m
@@ -0,0 +1,18 @@
+//
+//  MyCustomLayout.m
+//  Pods
+//
+//  Created by Brian Voong on 1/12/15.
+//
+//
+
+#import "MyCustomLayout.h"
+
+@implementation MyCustomLayout
+
+- (void)doWork
+{
+    NSLog(@"Doing work");
+}
+
+@end
