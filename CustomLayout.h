68e179b211684a463e7bbfc7f74449a0bec5db9c
Brian Voong <brian@touchofmodern.com>
2015-01-12 14:19:00 -0800

    CustomLayout class

---
+++ CustomLayout.h
@@ -0,0 +1,15 @@
+//
+//  CustomLayout.h
+//  Pods
+//
+//  Created by Brian Voong on 1/12/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CustomLayout : NSObject
+
+- (void)doCustomLayout;
+
+@end
---
+++ CustomLayout.m
@@ -0,0 +1,18 @@
+//
+//  CustomLayout.m
+//  Pods
+//
+//  Created by Brian Voong on 1/12/15.
+//
+//
+
+#import "CustomLayout.h"
+
+@implementation CustomLayout
+
+- (void)doCustomLayout
+{
+    NSLog(@"Doing custom layout");
+}
+
+@end
