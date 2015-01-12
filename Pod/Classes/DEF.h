588bb104f67dda97c08a031ad125c05b70e47d65
Brian Voong <brian@touchofmodern.com>
2015-01-12 15:00:42 -0800

    DEF

---
+++ Pod/Classes/DEF.h
@@ -0,0 +1,13 @@
+//
+//  DEF.h
+//  Pods
+//
+//  Created by Brian Voong on 1/12/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DEF : NSObject
+
+@end
---
+++ Pod/Classes/DEF.m
@@ -0,0 +1,13 @@
+//
+//  DEF.m
+//  Pods
+//
+//  Created by Brian Voong on 1/12/15.
+//
+//
+
+#import "DEF.h"
+
+@implementation DEF
+
+@end
